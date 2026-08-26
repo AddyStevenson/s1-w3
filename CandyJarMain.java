0ee0dc9515c908f47b5a4bea09c2114234684912
asdf <tasdlkfj>
2026-08-26 12:08:00 -0700

    candy jar

--- CandyJarMain.java
+++ CandyJarMain.java
@@ -2,18 +2,19 @@ public class CandyJarMain {
     public static void main(String[] args) {
       CandyJar a1 = new CandyJar();
       CandyJar a2 = new CandyJar(100);
-    }
 
-    a1.refill(20);
-    a2.eat(20);
+    a1.eat(20);
+    a2.refill(20);
     
 
-    a1.printstate();
-    a2.printstate();
+    a1.printState();
+    a2.printState();
 
   
-    c1.printState();
-    c2.printState();
+    a1.printState();
+    a2.printState();
+
+    }
 
 
 
