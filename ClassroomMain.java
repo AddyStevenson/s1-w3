e3242e6dac481c8b04869e695710f8579e970d09
asdf <tasdlkfj>
2026-08-28 11:17:56 -0700

    classroom

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,11 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        
+        Classroom c1 = new Classroom("Morris", 30, 30);
+        Classroom c2 = new Classroom("Kessler");
+
+        c2.setDesks(30);
+
+
+        System.out.println("Teacher: " + c1.teacher() + "Desks: " + c1.desks() + "Students: " + c1.students());
     }
 }
