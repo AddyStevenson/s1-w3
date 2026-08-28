8378df251336dac5fb5ed830f22b1c750de44f78
asdf <tasdlkfj>
2026-08-28 11:42:07 -0700

    score

--- Score.java
+++ Score.java
@@ -1,3 +1,26 @@
 public class Score {
+
+    private int points;
+
+    public Score(){
+        points = 50;
+    }
+
+    public Score(int p){
+        points = p;
+    }
+
+    private void gain(int pts){
+        points += pts;
+    }
+
+    private void lose(int pts){
+        points -= pts;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
     
 }
---
+++ ScoreMain.java
@@ -0,0 +1,10 @@
+public class ScoreMain {
+    public static void main(String[] args) {
+        Score s1 = new Score();
+        System.out.println(s1.getPoints());
+
+        Score s2 = new Score();
+        System.out.println();
+    }
+    
+}
